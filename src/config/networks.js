aeeaf0b22ddc7d7c756d999dcfa853fa00d1944e
GitHub Action <action@github.com>
2026-06-24 22:33:57 +0000

    init: project structure and network config

---
+++ src/config/networks.js
@@ -0,0 +1,6 @@
+export const NETWORKS = {
+  mainnet: { chainId: 1, rpc: "https://eth-mainnet.g.alchemy.com/v2/YOUR_KEY", name: "Ethereum" },
+  arbitrum: { chainId: 42161, rpc: "https://arb1.arbitrum.io/rpc", name: "Arbitrum One" },
+  optimism: { chainId: 10, rpc: "https://mainnet.optimism.io", name: "Optimism" },
+  base: { chainId: 8453, rpc: "https://mainnet.base.org", name: "Base" },
+}
